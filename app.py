25d3cd9c8fc339bbdbba37d802e01f1fd6f86cb2
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2025-01-16 17:30:19 +0000

    home, about, contact and services pages root completed

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
